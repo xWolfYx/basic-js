7352d9a3c703272959857b418920814abe01aab8
xWolfYx <david.skliarov@gmail.com>
2025-02-03 16:14:38 +0400

    feat: resolve get-email-domain.js

--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an email address, return it's domain.
@@ -10,11 +10,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getEmailDomain(email) {
+  // find the last index of "@"
+  const atIndex = email.lastIndexOf("@");
+  // slice the rest of the string
+  const domain = email.slice(atIndex + 1);
+  return domain;
 }
 
 module.exports = {
-  getEmailDomain
+  getEmailDomain,
 };
