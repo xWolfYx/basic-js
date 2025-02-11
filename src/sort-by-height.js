57c5a0de98a261062642b5e570109ebc87b11d6b
xWolfYx <david.skliarov@gmail.com>
2025-02-11 22:37:47 +0400

    feat: resolve sort-by-height.js

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -1,4 +1,5 @@
-const { NotImplementedError } = require('../extensions/index.js');
+// eslint-disable-next-line no-undef, no-unused-vars
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array with heights, sort them except if the value is -1.
@@ -11,11 +12,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  const numbers = arr.filter((num) => num !== -1);
+  const sortedNumbers = numbers.sort((a, b) => a - b);
+
+  const result = arr.map((num) =>
+    num === -1 ? -1 : sortedNumbers.shift()
+  );
+
+  return result;
 }
 
+// eslint-disable-next-line no-undef
 module.exports = {
-  sortByHeight
+  sortByHeight,
 };
