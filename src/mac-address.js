4271dba59999d4834643dcb263672615e20ca141
xWolfYx <david.skliarov.dev@gmail.com>
2025-11-08 16:08:12 +0400

    feat: implement isMAC48Address fn

--- src/mac-address.js
+++ src/mac-address.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * The MAC-48 address is six groups of two hexadecimal digits (0 to 9 or A to F),
@@ -14,11 +14,23 @@ const { NotImplementedError } = require('../lib');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function isMAC48Address(n) {
+  const addressArray = n.split("-");
+
+  if (addressArray.length !== 6) return false;
+
+  for (let pair of addressArray) {
+    if (pair.length !== 2) return false;
+
+    for (let i of pair) {
+      if (!((i >= "A" && i <= "F") || (i >= "0" && i <= "9"))) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
 
 module.exports = {
-  isMAC48Address
+  isMAC48Address,
 };
